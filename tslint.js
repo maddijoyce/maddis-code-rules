eed1385a91e46e1d097afc343b489ce4aae7b208
Maddi Joyce <maddi@maddijoyce.com>
2018-05-31 10:05:58 +1000

    Initial Commit

---
+++ tslint.js
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: ['tslint-config-airbnb', 'tslint-config-prettier'],
+  rules: {
+    'variable-name': [true, 'ban-keywords', 'check-format', 'allow-pascal-case'],
+    'function-name': [
+      true,
+      {
+        'method-regex': '^[a-z][\\w\\d]+$',
+        'private-method-regex': '^[a-z_][\\w\\d]+$',
+        'protected-method-regex': '^[a-z][\\w\\d]+$',
+        'static-method-regex': '^[a-z][\\w\\d]+$',
+        'function-regex': '^[a-z][\\w\\d]+$',
+      },
+    ],
+  },
+};
